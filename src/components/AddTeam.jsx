f92d355994b6492db230da427d63482a8e0cdd60
anan531 <ananyat612@gmail.com>
2026-07-14 22:17:53 +0530

    updated

--- src/components/AddTeam.jsx
+++ src/components/AddTeam.jsx
@@ -170,7 +170,7 @@ const AddTeam = () => {
 
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-<button className="btn btn-dark">SUBMIT</button>
+<button className="btn btn-dark" onClick={readValue}>SUBMIT</button>
 
         </div>
     </div>
